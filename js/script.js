760379c354c3626bffb41a969eccbcc253c819f2
1Hanif1 <hanifmohammed869@gmail.com>
2022-07-19 23:36:04 +0530

    javascript files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+const premiumServices = document.querySelectorAll('.premium');
+const premiumServiceHandler = (e) => {
+    if (!e.target.classList.contains('prev--js') && !e.target.classList.contains('next--js')) return;
+    const productContainer = e.target.closest('.premium_products').querySelector('.container');
+    if (e.target.classList.contains('prev--js')) {
+        productContainer.scrollLeft -= 200;
+        console.log(productContainer.scrollLeft);
+    }
+    else {
+        productContainer.scrollLeft += 200;
+        console.log(productContainer.scrollLeft);
+    }
+}
+premiumServices.forEach(service => {
+    service.addEventListener('click', premiumServiceHandler);
+})
+
+const numberSubmit = document.querySelector('.btn--js');
+const numberInput = document.querySelector('.number-input--js');
+const warningText = document.querySelector('.warning');
+numberSubmit.addEventListener('click', () => {
+    let regEx = /^\d{10}$/;
+    if (!numberInput.value.match(regEx)) {
+        warningText.style.display = 'block';
+        setTimeout(() => warningText.style.display = 'none', 1000);
+    }
+})
